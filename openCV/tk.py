357f6053ac760ca6a2f13bb1a7716774e30ecd9f
Mistyrain <Mistyrain@naver.com>
2022-12-27 14:44:50 +0900

    tk_final

--- openCV/tk.py
+++ openCV/tk.py
@@ -5,20 +5,21 @@ import pandas as pd
 from myDef import preprocessing
 # %%
 image,_=preprocessing(6)
-car_no='26차 2861'
+car_no='36서 9478'
 df=pd.read_csv('./info.csv',encoding='cp949')
-cols=['car_info', 'Patient_Id','Gender','exp1', 'exp2', 'exp3']
+cols=['car_info', 'Patient_Id','Gender','age','exp1', 'exp2', 'exp3']
 # %%
 def df_finder(car_no,df):
     if sum(df['car_info']==car_no) >=1:
         idx=df[df['car_info']==car_no].index.tolist()[0]
+        car=df.loc[idx,'car_info']
         P_id = df.loc[idx,'Patient_Id']
         gender = df.loc[idx,'Gender']
         age = df.loc[idx,'Age']
         exp1 = df.loc[idx,'exp1']
         exp2 = df.loc[idx,'exp2']
         exp3 = df.loc[idx,'exp3']
-        data_list=[P_id,gender,age,exp1,exp2,exp3]
+        data_list=[car,P_id,gender,age,exp1,exp2,exp3]
 
         return data_list
     else:
@@ -39,15 +40,15 @@ def open_window():
         img_label.configure(image=image_tk)
         img_label.grid(row=0,column=0)
 
-        frame=Frame(window,relief='solid',bd=2,background='white',padx=2,pady=2)
+        frame=Frame(window,relief='raised',bd=2,background='white',padx=2,pady=2)
         frame.grid(row=0,column=1)
         
         for i in enumerate(cols):
-            globals()["label{}".format(i[0])]=Label(master=frame, relief='ridge',bd=2,text=cols[i[0]],bg='white')
+            globals()["label{}".format(i[0])]=Label(master=frame,relief='solid',bd=1,width=15,height=2,text=cols[i[0]],bg='white')
             globals()["label{}".format(i[0])].grid(row=i[0],column=0)
         
         for i in enumerate(df_list):
-            globals()["label{}".format(i[0])]=Label(master=frame,text=df_list[i[0]],bg='white')
+            globals()["label{}".format(i[0])]=Label(master=frame,relief='ridge',bd=1,width=15,height=2,text=df_list[i[0]],bg='white')
             globals()["label{}".format(i[0])].grid(row=i[0],column=1)
 
     else:
@@ -57,4 +58,3 @@ def open_window():
     window.mainloop()
 # %%
 open_window()
-# %%
